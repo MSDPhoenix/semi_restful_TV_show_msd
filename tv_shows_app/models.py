001d8b2d43c7900bab8a1d502862872ee7cf1ec0
Michael S DuWors <46774296+MSDPhoenix@users.noreply.github.com>
2021-11-15 09:27:50 +0800

    validations added

--- tv_shows_app/models.py
+++ tv_shows_app/models.py
@@ -1,5 +1,25 @@
 from django.db import models
 from django.db.models.fields import DateTimeField,DateField
+from datetime import *
+
+class ShowManager(models.Manager):
+    def basic_validator(self,postData):
+        errors = {}
+        if postData['title'] == "" or postData['network'] == "" or postData['release_date'] == "" or postData['description'] == "":
+            errors['all_fields'] = "All fields are required"
+        if postData['title'] != "" and len(postData['title']) < 2:
+            errors['title'] = "Title should be at least 2 characters"
+        if postData['network'] != "" and len(postData['network']) < 3:
+            errors['network'] = "Network should be at least 3 characters"
+        if postData['description'] != "" and len(postData['description']) > 0 and len(postData['description']) < 10:
+            errors['description'] = "Description should be at least 10 characters"
+        if postData['release_date'] != "":
+            x = postData['release_date']
+            release_date = int(x[0:4]+x[5:7]+x[8:10])
+            today = int(datetime.today().strftime('%Y%m%d'))
+            if release_date > today:
+                errors['release_date'] = "Release date should be in the past"
+        return errors
 
 class Show(models.Model):
     title = models.CharField(max_length=50)
@@ -8,3 +28,4 @@ class Show(models.Model):
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects = ShowManager()
--- tv_shows_app/views.py
+++ tv_shows_app/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render,redirect
-from django.core import serializers,cache
+from django.shortcuts import render,redirect, HttpResponse
+from django.contrib import messages
 from .models import *
 
 def redirect_to_main_page(request):
@@ -9,19 +9,24 @@ def display_main_page(request):
     context = {
     'tv_shows' : Show.objects.all(),
     }
-
     return render(request,'main_page.html',context)
 
 def display_form_to_add_tv_show(request):
     return render(request,'add_show.html')
 
 def add_form_data_to_database(request):
-    title=request.POST['title']
-    network=request.POST['network']
-    release_date=request.POST['release_date']
-    description=request.POST['description']
-    Show.objects.create(title=title,network=network,release_date=release_date,description=description)
-    show_id = Show.objects.get(title=title,network=network,release_date=release_date,description=description).id
+    errors = Show.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+        for key,value in errors.items():
+            messages.error(request,value)
+        return redirect('/shows/new')
+    else:
+        title=request.POST['title']
+        network=request.POST['network']
+        release_date=request.POST['release_date']
+        description=request.POST['description']
+        Show.objects.create(title=title,network=network,release_date=release_date,description=description)
+        show_id = Show.objects.get(title=title,network=network,release_date=release_date,description=description).id    
     return redirect('/shows/'+str(show_id))
 
 def display_show_information(request,show_id):
@@ -33,6 +38,12 @@ def display_show_information(request,show_id):
         'release_date':show.release_date,
         'description':show.description,
         'updated_at':show.updated_at,
+
+        'title_type':str(type(show.title)),
+        'network_type':str(type(show.network)),
+        'release_date_type':str(type(show.release_date)),
+        'description_type':str(type(show.description)),
+        'updated_at_type':str(type(show.updated_at)),
     }
     return render(request,'show_information.html',context)
 
